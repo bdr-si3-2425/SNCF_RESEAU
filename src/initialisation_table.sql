1a8ef932c3528e50607af75234771f052595d0bc
Raphaël Laborie <raphael.laborie@gmail.com>
2025-02-02 19:08:01 +0100

    tqt meme pas

--- src/initialisation_table.sql
+++ src/initialisation_table.sql
@@ -9,12 +9,6 @@ CREATE TABLE incidents (
     description TEXT
 );
 
-CREATE TABLE lignes (
-    id_ligne SERIAL PRIMARY KEY,
-    FOREIGN KEY (terminus1) INT REFERENCES gares(id_gare),
-    FOREIGN KEY (terminus2) INT REFERENCES gares(id_gare)
-);
-
 CREATE TABLE trains (
     id_train SERIAL PRIMARY KEY,
     type VARCHAR(100) NOT NULL,
@@ -25,13 +19,23 @@ CREATE TABLE trains (
 CREATE TABLE villes (
 	id_ville INT PRIMARY KEY NOT NULL,
 	nom VARCHAR(100),
-	FOREIGN KEY (id_departement) INT REFERENCES departements(id_departement)
+	id_departement INT,
+	FOREIGN KEY (id_departement) REFERENCES departements(id_departement)
 );
 
 CREATE TABLE gares (
 	id_gare INT PRIMARY KEY NOT NULL,
 	nom VARCHAR(100),
-	FOREIGN KEY (id_ville) INT REFERENCES villes(id_ville)
+	id_ville INT,
+	FOREIGN KEY (id_ville) REFERENCES villes(id_ville)
+);
+
+CREATE TABLE lignes (
+    id_ligne SERIAL PRIMARY KEY,
+	terminus1 INT,
+	terminus2 INT,
+    FOREIGN KEY (terminus1) REFERENCES gares(id_gare),
+    FOREIGN KEY (terminus2) REFERENCES gares(id_gare)
 );
 
 CREATE TABLE equipements (
@@ -40,38 +44,47 @@ CREATE TABLE equipements (
 );
 
 CREATE TABLE equipements_gares (
+	id_gare INT,
+	id_equipement INT,
 	emplacement VARCHAR(255),
 	quantite_total INT,
 	quantite_operationelle INT,
-	FOREIGN KEY (id_gare) INT	REFERENCES gares(id_gare),
-	FOREIGN KEY (id_equipement) INT REFERENCES equipements(id_equipement)
+	FOREIGN KEY (id_gare) REFERENCES gares(id_gare),
+	FOREIGN KEY (id_equipement) REFERENCES equipements(id_equipement)
 );
 
 CREATE TABLE survenues_incidents (
+	id_gare INT,
+	id_ligne INT,
+	id_train INT,
+	id_incident INT,
     compte_rendu TEXT,
     impact VARCHAR(255),
     date_heure TIMESTAMP,
-    FOREIGN KEY (id_gare) INT REFERENCES gares(id_gare),
-    FOREIGN KEY (id_ligne) INT REFERENCES lignes(id_ligne),
-    FOREIGN KEY (id_train) INT REFERENCES trains(id_train),
-    FOREIGN KEY (id_incident) INT REFERENCES incidents(id_incident)
+    FOREIGN KEY (id_gare) REFERENCES gares(id_gare),
+    FOREIGN KEY (id_ligne) REFERENCES lignes(id_ligne),
+    FOREIGN KEY (id_train) REFERENCES trains(id_train),
+    FOREIGN KEY (id_incident) REFERENCES incidents(id_incident)
 );
 
 CREATE TABLE liaisons (
+	id_gare1 INT,
+	id_gare2 INT,
     date DATE,
     heure_depart_prevu TIME,
     heure_arrive_prevu TIME,
     heure_depart_reelle TIME,
     heure_arrive_reelle TIME,
-    FOREIGN KEY (id_gare1) INT REFERENCES gares(id_gare),
-    FOREIGN KEY (id_gare2) INT REFERENCES gares(id_gare),
+    FOREIGN KEY (id_gare1) REFERENCES gares(id_gare),
+    FOREIGN KEY (id_gare2) REFERENCES gares(id_gare)
 );
 
-CREATE TABLE maintenances {
+CREATE TABLE maintenances (
     id_cause INT PRIMARY KEY NOT NULL,
+	id_train INT,
     type VARCHAR(255),
     statut VARCHAR(255),
     date DATE,
     description TEXT,
-    FOREIGN KEY (id_train) INT REFERENCES trains(id_train)
-};
+    FOREIGN KEY (id_train) REFERENCES trains(id_train)
+);
