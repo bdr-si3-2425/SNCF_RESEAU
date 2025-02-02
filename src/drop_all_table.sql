52ff9ce443dbf1477ba5a65eb0cb20c1ea5b2236
Raphaël Laborie <raphael.laborie@gmail.com>
2025-02-02 19:16:58 +0100

    correction du drop table

--- src/drop_all_table.sql
+++ src/drop_all_table.sql
@@ -1,1 +1,3 @@
-DROP TABLE departements, lignes, incidents, trains, villes, gares, equipements, posseder, survenir, liaison, causes;
+DROP TABLE departements, lignes, incidents, trains, villes, gares, equipements, survenir, liaison, causes, posseder CASCADE;
+
+DROP TYPE gravite_type CASCADE;
