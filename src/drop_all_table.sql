df3871a07fc3ce1ec5a8af3ddfd711807ca105ce
Clément Menou <clementmenou0@gmail.com>
2025-02-03 19:43:00 +0100

    Fix drop tables (noms des tables)

--- src/drop_all_table.sql
+++ src/drop_all_table.sql
@@ -1,3 +1,3 @@
-DROP TABLE departements, lignes, incidents, trains, villes, gares, equipements, survenir, liaison, causes, posseder CASCADE;
+DROP TABLE departements, lignes, incidents, trains, villes, gares, equipements, survenues_incidents, liaisons, maintenances, equipements_gares CASCADE;
 
 DROP TYPE gravite_type CASCADE;
