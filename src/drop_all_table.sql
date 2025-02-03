f2769adeced3a40001a32dec89f3f680fa319535
Nic0lasmalay <nicolas.malay@gmail.com>
2025-02-03 22:05:46 +0100

    Modif drop_all_table

--- src/drop_all_table.sql
+++ src/drop_all_table.sql
@@ -1,3 +1,3 @@
-DROP TABLE departements, lignes, incidents, trains, villes, gares, equipements, survenues_incidents, liaisons, maintenances, equipements_gares CASCADE;
+DROP TABLE departements, lignes, incidents, trains, villes, gares, equipements, survenues_incidents, trajets, liaisons,lignes_laisons, maintenances, equipements_gares CASCADE;
 
 DROP TYPE gravite_type CASCADE;
