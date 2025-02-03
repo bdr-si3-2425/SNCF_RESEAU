d4992dcbed0c315db31f8e85453ca2816d5cb426
Nic0lasmalay <nicolas.malay@gmail.com>
2025-02-03 23:04:26 +0100

    Question 6 en cours ...

---
+++ src/question6.sql
@@ -0,0 +1,7 @@
+/* Un train peut être réaffecté si :
+
+    - Il est disponible : Il n'est pas déjà en circulation ou prévu pour un trajet imminent.
+    - Il est compatible avec la ligne : Il doit être adapté aux infrastructures (ex : trains à grande vitesse pour lignes TGV, trains régionaux pour les lignes locales).
+    - Il a une capacité suffisante : Pour éviter une surcharge de passagers.
+    - Il est proche géographiquement : Un train situé dans une gare à proximité peut être réaffecté plus rapidement.
+    - Il est en état opérationnel : Il ne doit pas être en maintenance ou signalé comme défectueux.*/
