5d63c9939e4ebfa5cdddc00abb96f246e43160ec
Raphaël Laborie <raphael.laborie@gmail.com>
2025-02-05 00:10:21 +0100

    ajout de la question2

--- src/drop_all_table.sql
+++ src/drop_all_table.sql
@@ -1,4 +1,3 @@
 DROP TABLE departements, lignes, incidents, trains, villes, gares, equipements, incidents_lignes, incidents_quais, incidents_gares, incidents_trains, trajets, liaisons, lignes_liaisons, maintenances, equipements_gares, incidents_maintenances, quais CASCADE;
 
 DROP TYPE GRAVITE_TYPE, TRAIN_TYPE CASCADE;
-
